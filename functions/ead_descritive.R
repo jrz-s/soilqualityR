09fb95fc5d96bb6d76a1158f937a0bcd0f156f19
Rafael Zarate <rzaratesalazar@gmail.com>
2026-05-09 17:19:34 -0300

    first commit

---
+++ functions/ead_descritive.R
@@ -0,0 +1,11 @@
+##################################################################
+#' Zárate-Salazar, J. Rafael 
+#' Agronomy | Biodiversity - MS | Soil Science - PhD
+#' PPEC - UFS
+#' rzaratesalazar@gmail.com
+##################################################################
+
+# -------------------------------------------------------------------------
+# Description
+
+#' Elaboração da primeira funcao do pacote soilqualityR.
---
+++ scripts/script01.R
@@ -0,0 +1,11 @@
+##################################################################
+#' Zárate-Salazar, J. Rafael 
+#' Agronomy | Biodiversity - MS | Soil Science - PhD
+#' PPEC - UFS
+#' rzaratesalazar@gmail.com
+##################################################################
+
+# -------------------------------------------------------------------------
+# Description
+
+#' Preparar o script para o primeira funcao do R.
